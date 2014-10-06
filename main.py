adabccda6b7fc91e9c71f922996a8f1b029ff46e
cutecprog <andi.grooms@gmail.com>
2014-10-06 12:55:33 -0500

    Completed verson 1 of question() and fixed doctest.

--- main.py
+++ main.py
@@ -8,15 +8,28 @@ def main():
                 print "Red Luna is a text adventure game."
                 return # Exit main()
         print "Red Luna"
+        print question("Ain't 'em pretty?", ["yes","no","the question is wrong"])
 
-def question(question, responses):
+def question(question, valid_responses, debug=False):
         """ Ask a question and wait for a valid response.
         
-        >>> question("Ain't 'em pretty?", ["yes","no","the question is wrong"])
-        2
+        >>> question("Ain't 'em pretty?", ["yes","no","the question is wrong"], True)
+        Ain't 'em pretty?
+        - yes
+        - no
+        - the question is wrong
+        'yes'
         """
-        return 0
+        print question
+        for valid_response in valid_responses:
+                print "-", valid_response
+        while True:
+                response = raw_input() if not debug else valid_responses[0]
+                if response in valid_responses:
+                        return response
 
 # Check if this file is being executed
 if __name__=="__main__":
+        from doctest import testmod
+        testmod()
         main() # run my function
