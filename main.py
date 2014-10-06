c204cec8d6eeb1adaee8586eb65b3a0d084f26c9
cutecprog <andi.grooms@gmail.com>
2014-10-06 12:01:04 -0500

    Added help command to main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #------------------------------------------------------------------------------
 # This file currently holds the entire game.
 #------------------------------------------------------------------------------
+
+from sys import argv
 def main():
+        if "-h" in argv or "--help" in argv:
+                print "Red Luna is a text adventure game."
+                return # Exit main()
         print "Red Luna"
 
 if __name__=="__main__":
