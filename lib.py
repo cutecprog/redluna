b3a8e822138882793136c849a9a2be8d7b0bc161
cutecprog <andi.grooms@gmail.com>
2014-11-06 04:19:15 -0600

    Initialized class hyper_word and method word.get_conotation()

--- lib.py
+++ lib.py
@@ -5,5 +5,16 @@
 class word(object):
         def __init__(self, synonym_list = []):
                 self.synonym_list = synonym_list
+
         def get_synonyms(self):
                 return ()
+
+        def get_conotation(self):
+                """ Return the feeling of the word as data.
+
+                """
+                return (0, 3, 2, 5)
+
+class hyper_word(word):
+        def __init__(self, synonym_list = []):
+                Base.__init__(self, synonym_list)
