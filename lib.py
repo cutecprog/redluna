82593b72f6a023c61e6247321ea4ff5309c29a1d
cutecprog <andi.grooms@gmail.com>
2014-11-15 15:28:35 -0600

    It was so satifying to delete all that code and knowing it's still safe. Starting to implement class prompt.

--- lib.py
+++ lib.py
@@ -2,19 +2,15 @@
 # Support library for the game
 #-------------------------------------------------------------------------------
 
-class word(object):
-        def __init__(self, synonym_list = []):
-                self.synonym_list = synonym_list
-
-        def get_synonyms(self):
-                return ()
-
-        def get_conotation(self):
-                """ Return the feeling of the word as data.
-
-                """
-                return (0, 3, 2, 5)
-
-class hyper_word(word):
-        def __init__(self, synonym_list = []):
-                Base.__init__(self, synonym_list)
+class prompt(object):
+        def __init__(self, text):
+                self.text = text
+                index = 0
+                self.links = []
+                for ch in text:
+                        if ch == '[':
+                                self.links.append("")
+                        elif ch == ']':
+                                index += 1
+                        elif len(self.links) > index:
+                                self.links[index] += ch
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # This file currently holds the entire game
 #-------------------------------------------------------------------------------
 
+from lib import prompt
+
 def main(args, debug=False):
         """ Print args and if not in debug mode start the game.
 
@@ -16,34 +18,7 @@ def main(args, debug=False):
         if debug:
                 return "what is returned when debug is True" # exit main()
 
-        # the game
-        print question("Ain't 'em pretty?", ["yes","no","mayhaps"])
-        print question("What's your favoriate color?", ["scarlet", "violet",   \
-                                                "yellow", "torquoise", "aqua", \
-                                                "orange", "blue"])
-        # exit main()
-
-def question(question, valid_responses, debug=False):
-        """ Ask a question and wait for a valid response.
-
-        >>> question("Ain't 'em pretty?", ["yes","no","maybe"], True)
-        Ain't 'em pretty?
-        - yes
-        - no
-        - maybe
-        'yes'
-
-        """
-        print question
-        # print valid responses
-        for valid_response in valid_responses:
-                print "-", valid_response
-        while True: # valid input loop
-                # get user input if not in debug mode
-                # else use first item from valid_response list.
-                response = raw_input() if not debug else valid_responses[0]
-                if response in valid_responses:
-                        return response # exit question()
+        # end game
 
 # if this file is being executed 
 # then check for commend line args, run doctests, run main()
