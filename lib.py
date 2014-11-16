68bc590216ddd04241539dcea02e498b408e6ce6
cutecprog <andi.grooms@gmail.com>
2014-11-15 19:23:39 -0600

    Code fully ported over but not tested

--- lib.py
+++ lib.py
@@ -3,9 +3,28 @@
 #-------------------------------------------------------------------------------
 
 from time import time
+from re import compile, match
+
+command_list = compile('(^forsake game stint)\s*$|(^spare)\s*$|(^spare as )(\w+)\s*$')
 
 def print_loc(text, y, x):
         print("\033[%s;%sH" % (y,x) + text)
+
+def getch():
+        import tty, termios, sys
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+                tty.setraw(sys.stdin.fileno())
+                ch = sys.stdin.read(1)
+        finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+def save(filename = str(int(time()))):
+        with open(filename, 'w') as f:
+                f.write('null')
+        exit()
 
 class prompt(object):
         def __init__(self, y, x, text, start_head = 0, start_tail = 5):
@@ -33,6 +52,7 @@ class prompt(object):
                 self.tail_x     = 0
                 self.tail_y     = 0
                 self.text_color = '\033[0m'
+                self.user_input = ""
 
         def head_pass(self):
                 self.head_start_time = time()
@@ -75,3 +95,84 @@ class prompt(object):
                         print_loc("\033[0m ", self.y + self.tail_y, self.x + self.tail_x)
                         self.tail += 1
                         self.tail_x += 1
+
+        def onKeyPress(self):
+                """ Get a character and change user input line accordingly.
+
+                """
+                ch = getch()
+                if ch == '\x1b':                # escape
+                        exit()
+                if ch == '\r':                  # return
+                        if self.user_input == "":
+                                return
+                        command = command_list.match(self.user_input)
+                        if not command:
+                                pass
+                        elif command.group(1):
+                                exit()
+                        elif command.group(2):
+                                save()
+                        elif command.group(3):
+                                save(command.group(4))
+                        self.user_input = ""
+                        print '\033[0m'
+                        print_loc(' '*80, self.y+5, self.x+2)
+                elif ch == '\x7f':              # backspace
+                        if self.user_input == "":
+                                return
+                        self.user_input = self.user_input[:-1]
+                elif ch == ' ':                 # space
+                        if self.user_input == "":
+                                return
+                        self.user_input += ' '
+                elif len(self.user_input) >= 80:           # too long
+                        return
+                else:                           # all else
+                        self.user_input += ch
+                # Highlight valid user input
+                if self.user_input.lower() in self.links:
+                        print '\033[0m\033[96m\033[4m'
+                elif command_list.match(self.user_input):
+                        print '\033[0m\033[1m\033[92m'
+                else:
+                        print '\033[0m'
+                # Display new user input line
+                print_loc(self.user_input+'\033[0m\033[1m < ', self.y + 5, self.x)
+        def display(self):
+                # Prompt box
+                print '\033[4m'
+                print '\033[1m'
+                print_loc(' '*82, self.y-1, self.x-1)
+                print_loc('Old man', self.y-1, self.x)
+                print '\033[0m'
+                print '\033[1m'
+                print_loc('|',  self.y, self.x-2)
+                print_loc('|',  self.y, self.x+81)
+                print_loc('|',  self.y+1, self.x-2)
+                print_loc('|',  self.y+1, self.x+81)
+                print_loc('|',  self.y+2, self.x-2)
+                print_loc('|',  self.y+2, self.x+81)
+                print_loc('|',  self.y+3, self.x-2)
+                print_loc('|',  self.y+3, self.x+81)
+                print_loc('|',  self.y+4, self.x-2)
+                print_loc('|',  self.y+4, self.x+81)
+                print '\033[4m'
+                print_loc(' '*82, self.y+4, self.x-1)
+                print '\033[0m'
+                print '\033[1m'
+                print_loc('>  <', self.y+5, self.x-2)
+                print '\033[0m'
+                print_loc('\033[1mCommands:\033[0m', 10, 20)
+                print '\033[0m\033[1m\033[92m'
+                print_loc('forsake game stint', 11, 20)
+                print_loc('spare', 12, 20)
+                print_loc('spare as [filename]', 13, 20)
+                print '\033[0m'
+                print_loc('quit without saving', 11, 48)
+                print_loc('save and quit', 12, 48)
+                print_loc('save to filename and quit', 13, 48)
+
+                print_loc('Press a key to start', self.y+2, self.x+15)
+                getch()
+                print_loc('                    ', self.y+2, self.x+15)
--- main.py
+++ main.py
@@ -5,35 +5,45 @@
 from lib import prompt
 import multiprocessing
 from time import time
+from atexit import register  # For clean up function
+from os import system
 
-def main(args, debug=False):
-        """ Print args and if not in debug mode start the game.
-
-        >>> main("This could be anything", True)
-        This could be anything
-        'what is returned when debug is True'
-
-        """
-        print args
-        # if debug mode, return a meaningless string that the doctest expects
-        if debug:
-                return "what is returned when debug is True" # exit main()
-
-        story = prompt(20, 60,\
+story = prompt(20, 60,\
 """Long ago when the [moon shone true] a [girl] like you climbed that tree. Her hair
 wasn't as fair and she wasn't nearly as inquisitive of her elder folk. [She died].""")
 
-        story.head_pass()
+def main():
+        global story
+        init()
+        story.display()
+        story.prompt_time = time()
+        loop_process.start()
+        while True:
+                story.onKeyPress()
+
+def init():
+        print '\033[0m'
+        system('setterm -cursor off')
+        system('clear')
+
+def loop():
+        global story
         while True:
                 if time()-story.head_start_time > .1:
                         story.head_pass()
                 if time() - story.prompt_time > 5.0 and                        \
                                         time()-story.tail_start_time > .09:
                         story.tail_pass()
-        # end game
+loop_process = multiprocessing.Process(name='loop', target=loop)
 
-# if this file is being executed 
-# then check for commend line args, run doctests, run main()
+# Exit code
+@register
+def goodbye():
+        print '\033[0m'
+        system('setterm -cursor on')
+        loop_process.terminate()
+        system('clear')
+
 if __name__ == "__main__":
         from sys import argv
         if "-h" in argv or "--help" in argv:
@@ -41,7 +51,4 @@ if __name__ == "__main__":
                 with open("README", "r") as readme_file:
                         print readme_file.read()
                 exit()
-
-        from doctest import testmod
-        testmod() # run all doctest
-        main("Red Luna") # run my function
+        main()
